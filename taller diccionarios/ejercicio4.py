597430f8fa0b61ec8e739a199974824db441fc3c
S-Cardozo123 <scardo21123@universidadean.edu.co>
2022-11-16 17:59:23 -0500

    taller diccionarios

--- taller diccionarios/ejercicio4.py
+++ taller diccionarios/ejercicio4.py
@@ -3,14 +3,25 @@ estudiantes = { "1": { "nombre": "Lorea", "nota": 8 },
 "3": { "nombre": "Julen", "nota": 6.5 } } 
 N=0
 canestu=int(input("cantidad estudiantes: "))
+estu={}
+aprovados=[]
+suspendidos=[]
+t=0
 while True:
-    estudiante1={}
-    estudiantes={}
     N=N+1
     b=str(input("nombre: "))
-    a=int(input("nota: "))
-    estudiante1.update({b:a})
-    estudiantes[N]=estudiante1
+    a=float(input("nota: "))
+    dicc={N:{"nombre": b, "nota": a}}
+    estu.update(dicc)
+    t=t+a
+    if(a>=6):
+        aprovados.append(b)
+    if(a<6):
+        suspendidos.append(b)
     if(N==canestu):
         break
-print(estudiantes)
+prom=(t/canestu)
+print("diccionario",estu)
+print("aprovados",aprovados)
+print("suspendidos: ",suspendidos)
+print(f"promedio: {prom:.2f}")
